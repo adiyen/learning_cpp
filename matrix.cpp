6b4c7ed1f6c83e3a8406860a1dab76e077d1a4cf
shobanavv <shobanavv@gmail.com>
2018-03-25 06:27:24 -0700

    done

--- matrix.cpp
+++ matrix.cpp
@@ -2,24 +2,61 @@
 using namespace std;
 
 int main() {
-    int n[] = {};
-    cin >> n[0] >> n[1];
-    int numb[] = {};
+    int rows, cols;
+    cin >> rows >> cols;
+    int numb[rows][cols];
 
+    // for(int j = 0; j < n[0]; j++) {
+    //     i++;
+    //     for(int k = 0; k < n[1]; k++){
+    //         i++;
+    //         cin >> numb[i];
+    //     }
+    // }
 
-    for(int j = 0; j < n[0]*n[1]; j++) {
-            cin >> numb[j];
+    // for(int i = 0; i < n[0]; i++) {
+    //     for (int k = 0; k < n[1]; k++) {
+    //         cout << numb[k]*numb[k] << " "; 
+    //     }
+    //     //cout << endl;
+    // }
+
+    for(int k = 0; k < rows; k++) {
+        for(int l = 0; l < cols; l++){
+            cin >> numb[k][l];
+        }
     }
-
-    for(int i = 0; i < n[0]; i++) {
-        for (int k = 0; k < n[1]; k++) {
-            cout << numb[k]*numb[k] << " "; 
+    cout << endl;
+    for(int n = 0; n < rows; n++) {
+        for(int m = 0; m < cols; m++){
+            cout << numb[n][m] << " ";
         }
         cout << endl;
     }
 
-    // for(int i = 3; i < n[0]*n[1]; i++) {
-    //     cout << numb[i]*numb[i] << " ";
+    // for(int j = 0; j < n[0]*n[1]+1; j++) {
+    //     cin >> numb[j];
     // }
-    cout << endl;
+
+    // for(int m = 0; m < n[0]*n[1]; m++){
+    //     cout << numb[m] << endl;
+    // }
+//     for(int i = 0; i < n[0]*n[1]; i++) {
+//         cout << numb[i]*numb[i] << " ";
+//     }
+//     cout << endl;
+// }
+
+//Question: I have to make a 2 -d array and i have to output the n*n for each array
+
+//example:
+//3 4
+//1 2 2 1
+//3 4 4 3
+//1 3 3 1
+
+//Output
+//1 4 4 1
+//9 16 16 9
+//1 9 9 1
 }
---
+++ pacman.cpp
@@ -0,0 +1,37 @@
+// #include <fstream>
+// using namespace std;
+// int m;
+// string line;
+// int n = line.length();
+// char arr[m][n];
+// for(int j = 0; j< m; j++){
+//     for (int i = 0; i < n; i++){
+//         fin >> line;
+//         arr[i][j] = line[i];
+//     }
+// }
+
+#include <fstream>
+using namespace std;
+
+int main() {
+    if()
+}
+
+
+if(getch(a)) {
+    player_pos = x;
+    player_pos = y - 1;
+}
+if(getch(d)) {
+    player_pos = x;
+    player_pos = y + 1;
+}
+if(getch(w)) {
+    player_pos = x + 1;
+    player_pos = y;
+}
+if(getch(s)) {
+    player_pos = x - 1;
+    player_pos = y;
+}
