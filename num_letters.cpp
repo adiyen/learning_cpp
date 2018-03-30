b897cdfdd36c733e0b9908ec21297b4e7167a721
shobanavv <shobanavv@gmail.com>
2018-03-30 14:55:59 -0700

    done

---
+++ num_letters.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int NumLetters(string word) {
+
+}
+
+int main() {
+    string my_word;
+    cin >> my_word;
+    NumLetters(my_word);
+
+}
